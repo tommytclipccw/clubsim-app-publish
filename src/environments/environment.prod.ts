b27528e51a0af437447ea4fd263830bd96197b0a
Tommy.tc.li <tommy.tc.li@pccw.com>
2019-02-22 16:05:10 +0800

    FIX: GHP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'https://www.tommyltc.com/_/',
+  endpoint: 'https://localhost/_/',
   firebase: {
     apiKey: 'AIzaSyD4K_HT0mYdkBsFgtFh9yoNQdJlplFAYwo',
     authDomain: 'clubsim-tools.firebaseapp.com',
